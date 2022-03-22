634450b5f20c3d67cfd38bc9eb80ab4b1a8a9cbd
Cesar T. Putra <cesartputra@Cesars-MacBook-Pro.local>
2022-03-22 19:18:35 +0700

    add header mockup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import Head from 'next/head'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '../styles/custom.css'
 import '../styles/globals.css'
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // return <Component {...pageProps} />
+  useEffect(() => {
+      import("bootstrap/dist/js/bootstrap.min.js");
+  }, []);
+
+  return (
+    <>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
